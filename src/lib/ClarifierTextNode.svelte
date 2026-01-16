ee8f9f79898c552ee816cf5a2ea47d9d13b1b587
Gabriel Gaytan <ggayta15@my.epcc.edu>
2026-01-16 16:51:16 -0700

    hi izzyyyy

--- src/lib/ClarifierTextNode.svelte
+++ src/lib/ClarifierTextNode.svelte
@@ -5,15 +5,36 @@
     let { updateNodeData } = useSvelteFlow();
 </script>
 
-<div class="size-fit border rounded-xl bg-black p-6">
-    <label for="text">Text:</label>
-    <input
-        id="text"
-        name="text"
-        value={data.text}
-        oninput={(e) => {
-            updateNodeData(id, { text: e.currentTarget.value });
-        }}
-        class="nodrag"
-    />
-</div>
+<!-- title -->
+ <div class="border-5 rounded ">
+     <div class="text-white bg-gray-500 p-1">
+         <label for="title">Title:</label>
+         <input
+             id="text"
+             name="title"
+             value={data.text}
+             oninput={(e) => {
+                 updateNodeData(id, { text: e.currentTarget.value });
+             }}
+             class="nodrag2"
+         />
+         <!-- layer button -->
+         <button class="text-black bg-white border-2 rounded p-1"> 
+            lyr
+        </button>
+     
+         <!-- textbox -->
+         <div class="size-fit border rounded-xl text-black bg-white p-6">
+             <label for="text">Text:</label>
+             <input
+                 id="text"
+                 name="text"
+                 value={data.text}
+                 oninput={(e) => {
+                     updateNodeData(id, { text: e.currentTarget.value });
+                 }}
+                 class="nodrag"
+             />
+         </div>
+     </div>
+ </div>
